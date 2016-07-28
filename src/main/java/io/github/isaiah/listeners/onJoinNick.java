7254d22b70aeea8ce9b69987e28156cb93036a77
Isaiah Patton <isaiahpatton1@gmail.com>
2016-07-28 15:25:30 -0400

    Create onJoinNick.java

---
+++ src/main/java/io/github/isaiah/listeners/onJoinNick.java
@@ -0,0 +1,23 @@
+package io.github.isaiah.listeners;
+
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+import io.github.isaiah.bssentials.Bssentials;
+
+public class onJoinNick implements Listener {
+
+	private Bssentials main;
+	
+	@EventHandler
+	public void onPlayerJoin(PlayerJoinEvent e) {
+		Player player = e.getPlayer();
+		if (main.getConfig().getString("playerdata." + player.getName() + ".nick") != null) {
+			player.setDisplayName(main.getConfig().getString("playerdata." + player.getName() + ".nick"));
+			player.sendMessage(Bssentials.PREFIX + ChatColor.GOLD + "Found a nick name you set! type /disnick to disable your nickname!");
+		}
+	}
+}
