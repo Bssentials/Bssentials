80464bc205ffc2a29a8d98704fb1c4312a20a59c
Isaiah Patton <isaiahpatton1@gmail.com>
2016-09-10 22:16:27 -0400

    > Create Addon Manager <

---
+++ src/ml/bssentials/addons/AddonManager.java
@@ -0,0 +1,29 @@
+package ml.bssentials.addons;
+
+public class AddonManager {
+	/*
+	 * Forget that you ever saw this!!!
+	 * Nothing to see here!!!
+	 * Click the BACK button NOW!!!
+	 * I MEAN IT!!!
+	 * CLICK BACK!!!
+	 * DO NOT READ THE TITLE OF THIS FILE!!!!
+	 * FORGET THERES AN ADDON MANAGER!!!
+	 * FORGET THERES EVEN AN PACKAGE NAMED ml.bssentials.addons !!!
+	 * 
+	 * Thank you :)
+	 * 
+	 * - Bssentials Team
+	 * 
+	 * 
+	 * AND IF YOU CARE ABOUT BEING ALIVE THEN DO NOT READ THE FALLOWING LINES:
+	 *
+	 *	Addon Manager ideas:
+	 *		- Allow Bukkit to run Sponge plugins!
+	 *		- Allow Bukkit to run Rainbow plugins!
+	 *		- Make an BIG API to allow users to make custom Bssentials Addons!
+	 *		- Kill every body who read this far because I said: 'If you care about being alive to not read this'
+	 *		- The end :)
+	 *
+	 * */
+}
