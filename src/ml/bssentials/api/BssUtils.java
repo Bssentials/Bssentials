d7b5536573ce8f4fcad8f1bc877f5866521d181e
Isaiah Patton <isaiahpatton1@gmail.com>
2016-10-06 16:31:09 -0400

    Add broadcastMessage(); to BssUtils

--- src/ml/bssentials/api/BssUtils.java
+++ src/ml/bssentials/api/BssUtils.java
@@ -1,6 +1,7 @@
 package ml.bssentials.api;
 
 import org.bukkit.entity.Player;
+import org.bukkit.Bukkit;
 
 import io.github.ramidzkh.utils.PlayerCheck;
 
@@ -23,6 +24,13 @@ public class BssUtils {
 	 * */
 	public static void noPermMsg(Player p) {
 		p.sendMessage("No permisson!");
+	}
+	
+	/**
+	 * Broadcast an message to the server
+	 **/
+	public static void broadcastMessage(Object message) {
+		Bukkit.broadcastMessage(message);
 	}
 
 }
