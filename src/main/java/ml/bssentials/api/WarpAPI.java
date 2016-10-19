12232cec5721b255c84d016ce54a44ef71ded20e
Isaiah Patton <isaiahpatton1@gmail.com>
2016-10-19 12:22:09 -0400

    Update WarpAPI.java

--- src/main/java/ml/bssentials/api/WarpAPI.java
+++ src/main/java/ml/bssentials/api/WarpAPI.java
@@ -1,22 +1,5 @@
 package ml.bssentials.api;
 
-import ml.bssentials.main.Bssentials;
-
+@Deprecated
 public class WarpAPI {
-	public static Bssentials main;
-	
-    @Deprecated
-    public static void createWarp(Player p, String warpname) {
-        Bssentials.createWarp(p,warpname);
-    }
-    
-    @Deprecated
-    public static void createHome(Player p) {
-        Bssentials.createHome(p);
-    }
-    
-    @Deprecated
-    public static void delHome(Player p) {
-    	Bssentials.delHome(p);
-    }
 }
